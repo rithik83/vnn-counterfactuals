21f612b84bb7266bcd62aa362f37d25de5f20153
Rithik Appachi Senthilkumar <rithiksenthilkumar@outlook.com>
2024-12-14 22:37:56 -0500

    README

--- jl_counterfactual_generation/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import os
-
-print(os.getcwd())
-
-data = np.loadtxt("jl_counterfactual_generation/data.csv", delimiter=',', skiprows=1)
-
-print("size of data: ", data.shape)
-print("data first pt: ", data[0])
